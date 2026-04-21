bb4eb22cdb10c3a08589aaabdbdcde6dca4115be
sofiyk95-hash <sofi.yk95@gmail.com>
2026-04-21 17:29:24 +0000

    create js file

---
+++ .js
@@ -0,0 +1,47 @@
+// Messi Goals
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// Make Negative
+
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+}
+
+// Game Move
+
+function move (position, roll) {
+  return position + roll * 2;
+}
+
+// Personalized Message
+
+function greet (name, owner) {
+  if (name === owner) {
+    return 'Hello boss';
+  } else {
+    return 'Hello guest';
+  }
+}
+
+// Keep Hydrated
+
+function liters(time) {
+  return Math.floor(time * 0.5);
+}
+
+// Opposites Attract
+
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 !== flower2 % 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
