fed0b763a7a9b7f57cd186b24b62db559c642c02
sofiyk95-hash <sofi.yk95@gmail.com>
2026-04-27 14:45:12 +0300

    Add giveMeFive function to filter keys and values

--- .js
+++ .js
@@ -58,3 +58,16 @@ return string.split(" ");
 
 
 }
+// Add giveMeFive function to filter keys and values
+function giveMeFive(obj) {
+ let result = [];
+for (let key in obj) {
+    if (key.length === 5) {
+      result.push(key);
+    }
+    if (obj[key].length === 5) {
+      result.push(obj[key]);
+    }
+  }
+  return result;
+}
